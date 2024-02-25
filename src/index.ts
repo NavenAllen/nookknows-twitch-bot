d0cff2c426f6c5778adaf390ab17835989e3c958
NavenAllen <naven1999@gmail.com>
2024-02-25 04:58:35 -0700

    feat: add mod actions for force joining/leaveing

--- src/index.ts
+++ src/index.ts
@@ -30,21 +30,47 @@ function onMessageHandler(
   msg: string,
   self: Boolean
 ) {
-  if (self) {
-    return;
-  } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
   const command = msg.split(' ');
 
+  if (self) {
+    return
+  }
+
+  console.log(channel)
   // If the command is known, let's execute it
-  if (command[0] === '!join') {
-    client.join(context.username!)
-    console.log(`* JOINED ${context.username!}`);
-  } else if (command[0] === '!leave') {
-    client.part(context.username!)
-    console.log(`* LEFT ${context.username!} :(`);
-  } else if (command[0] === '!villager') {
+  if(channel === '#nookknows') {
+    if(command[0] === '!modaction') {
+      const moderators = process.env.MODERATORS?.split(",")
+      if(moderators!.includes(context.username!)) {
+        if (command[1] === 'join') {
+          if (command.length !== 3) {
+            client.say(channel, 'Usage: !modaction join <channel_name>');
+          } else {
+            client.join(`#${command[2].toLowerCase()}`)
+          }
+          console.log(`* JOINED ${command[1]}`);
+        } else if (command[1] === 'leave') {
+          if (command.length !== 3) {
+            client.say(channel, 'Usage: !modaction leave <channel_name>');
+          } else {
+            client.part(`#${command[2].toLowerCase()}`)
+          }
+          console.log(`* LEFT ${command[1]} :(`);
+        } 
+      } else {
+        client.say(channel, "You do not have the permission to do that!!")
+      }
+    } else if (command[0] === '!join') {
+      client.join(context.username!)
+      console.log(`* JOINED ${context.username!}`);
+    } else if (command[0] === '!leave') {
+      client.part(context.username!)
+      console.log(`* LEFT ${context.username!} :(`);
+    }
+    return
+  }
+
+  if (command[0] === '!villager') {
     if (command.length !== 2) {
       client.say(channel, 'Usage: !villager <villager_name>');
     } else {
